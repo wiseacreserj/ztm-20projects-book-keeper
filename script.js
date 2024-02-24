01cf88275f5e04264ac129c09b10390d620aa3e0
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2024-02-25 00:06:39 +0200

    done JS for validation form

--- script.js
+++ script.js
@@ -22,3 +22,40 @@ modalClose.addEventListener("click", () => {
 window.addEventListener("click", (event) => {
     event.target === modal ? modal.classList.remove("show-modal") : false;
 });
+
+const validate = (nameValue, urlValue) => {
+    const expression =
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert("Please submit values for both fields");
+        return false;
+    }
+    if (urlValue.match(regex)) {
+        alert("match");
+        return true;
+    }
+
+    if (!urlValue.match(regex)) {
+        alert("Please provide a valud web address");
+        return false;
+    }
+};
+
+// Handle data from form
+
+const storeBookmark = (event) => {
+    event.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes("http://", "https://")) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+};
+
+//Event listeners
+bookmarkForm.addEventListener("submit", storeBookmark);
